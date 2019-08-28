fd0931c334a04135b99bd0c6dd15115a0c6531bd
arpanmangal <mangalarpan@gmail.com>
2019-08-28 23:26:58 +0530

    Function for contradiction

--- A1/tableaux.ml
+++ A1/tableaux.ml
@@ -58,6 +58,14 @@ let nodeClosed aNode = {
 let nodeDesc aNode num_desc = {
     aNode with num_desc = num_desc;
 };;
+let getNodeProp aNode = 
+    let aNodeVal = aNode.value in
+    match aNodeVal with Value (p, t) -> p
+;;
+let getNodeTruth aNode = 
+    let aNodeVal = aNode.value in
+    match aNodeVal with Value (p, t) -> t
+;;
 
 
 (* definition of a tableaux *)
@@ -66,5 +74,66 @@ let emptyTab (_ : string) : node = (newNode T false);; (* Can change the default
 let addNodeInTab aTab key p truth = fun key' -> if key' = key then (newNode p truth) else aTab key;;
 
 
-
 (* Developing the tableaux *)
+(* Helper functions *)
+let concat s1 s2 =
+    String.concat s1 ["";s2];;
+
+let nList n = 
+    if n = 0 then [] else
+    let rec createList mx l = if mx <= 0 then l
+        else 
+        let mx1 = mx - 1 in
+        match l with
+        | [] -> createList mx1 [mx1]
+        | x::xs -> createList mx1 (mx1::l)
+    in
+    createList n []
+    ;;
+
+let allPrefixes s = 
+    let sLen = String.length s in
+    if sLen = 0 then [] else
+    if sLen = 1 then [""] else
+    List.map (String.sub s 0) (nList sLen)
+;;
+
+(* 1. Closing all paths from this node *)
+let rec closePath aTab nodeID = let currNode = aTab nodeID
+    in
+    let _currClosedNode = nodeClosed currNode
+    in
+    if currNode.num_desc = 0 then () else
+        let _closeLeft = closePath aTab (concat nodeID "0")
+        in
+        if currNode.num_desc = 2 then 
+            closePath aTab (concat nodeID "1")
+        else
+            ()
+    ;;
+
+(* 2. Closing all the contradicting paths *)
+let contrad_path aTab =
+    let isContradiction nodeID1 nodeID2 =
+        let node1 = aTab nodeID1 in
+        let node2 = aTab nodeID2 in 
+        if (((getNodeProp node1) = (getNodeProp node2)) && ((getNodeTruth node1) <> (getNodeTruth node2)))
+        then true else false
+    in
+    let rec closeContradPaths aTab nodeID = 
+        let currNode = aTab nodeID in
+        let parents = allPrefixes nodeID in
+        let f truth aNodeID = truth || (isContradiction aNodeID nodeID) in
+        let isContraNode = (List.fold_left f false parents) in
+        if isContraNode then closePath aTab nodeID else
+            if currNode.num_desc = 0 then () else
+                let _contraLeft = closeContradPaths aTab (concat nodeID "0")
+                in
+                if currNode.num_desc = 2 
+                then
+                    closeContradPaths aTab (concat nodeID "1")
+                else
+                    ()
+    in
+    closeContradPaths aTab ""
+;;
